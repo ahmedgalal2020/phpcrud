b70b6c9e34b15d18fbee1600d732430acc623479
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-08 13:41:38 +0100

    update 314

--- index.php
+++ index.php
@@ -4,33 +4,6 @@
 ?>
 
 
-<?php include_once 'parts/header.php'; ?>
-<div class="custom-bg">
-<div class="container ">
-<div class="row ">
-
-
-<div class="col-6 ">
-<div class="col-4 m-1 rounded-5 p-4 border border-primary">hi</div>
-    <div class="col-4 m-1 rounded-4 p-4 border border-primary">hi</div>
-    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
-    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
-    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
-    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
-
-</div>
-<div class="col-6 ">
-<div class="col-4 m-1 rounded-5 p-4 border border-primary">hi</div>
-    <div class="col-4 m-1 rounded-4 p-4 border border-primary">hi</div>
-    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
-    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
-    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
-    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
-</div>
-
-</div>
-</div>
-</div>
 
 
 
