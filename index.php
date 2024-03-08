ab2458f0d7b3ed004734eaef0bbf0673a2b03511
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-08 11:52:03 +0100

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <?php include_once 'parts/header.php'; ?>
 
-<div class="row mt-4 text-center justify-center">
+<div class="row mt-4 text-center d-flex justify-content-center align-items-center">
     <div class="col-4 m-2 rounded-5 p-4 border border-primary">hi</div>
     <div class="col-4 m-2 rounded-5 p-4 border border-primary">hi</div>
     <div class="col-4 m-2 rounded-5 p-4 border border-primary">hi</div>
