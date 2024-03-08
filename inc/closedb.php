9ad4a4a70936f15d302e5c03616ff2b619e409d4
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-08 14:26:00 +0100

    upfate aft

--- inc/closedb.php
+++ inc/closedb.php
@@ -1,3 +1,9 @@
 <?php
-mysqli_free_result($result);
+// Only free result if it exists and is a valid result set
+if (isset($result) && $result instanceof mysqli_result) {
+    mysqli_free_result($result);
+}
+
+// Always close the connection
 mysqli_close($conn);
+
--- inc/db.php
+++ inc/db.php
@@ -1,3 +1,8 @@
 <?php
 // Database connection
 $conn = mysqli_connect('localhost', 'u294421851_phpcrud', 'Am13111991', 'u294421851_phpcrud');
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
