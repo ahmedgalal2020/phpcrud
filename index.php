e520f2069a0877392e6adeaca5e279e35a3f9acf
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-08 11:58:09 +0100

    update

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
-<?php 
+<?php
 
 
 ?>
 
 
 <?php include_once 'parts/header.php'; ?>
+<div class="row">
+<div class="col-4">
+<div class="col-4 m-1 rounded-5 p-4 border border-primary">hi</div>
+    <div class="col-4 m-1 rounded-4 p-4 border border-primary">hi</div>
+    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
+    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
+    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
+    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
 
+</div>
+<div class="col-4">
+
+<div class="col-4 m-1 rounded-5 p-4 border border-primary">hi</div>
+    <div class="col-4 m-1 rounded-4 p-4 border border-primary">hi</div>
+    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
+    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
+    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
+    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
+</div>
+
+</div>
 <div class="row mt-4 text-center d-flex justify-content-center align-items-center">
     <div class="col-4 m-1 rounded-5 p-4 border border-primary">hi</div>
     <div class="col-4 m-1 rounded-4 p-4 border border-primary">hi</div>
