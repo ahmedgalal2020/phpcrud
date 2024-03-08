c80c251a0b51b2e4fa17c499891d7c8d6678d4f3
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-08 11:45:56 +0100

    update

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 <?php include_once 'parts/header.php'; ?>
 
 <div class="row mt-4 text-center">
-    <div class="col-3 ">hi</div>
-    <div class="col-3">hi</div>
-    <div class="col-3">hi</div>
-    <div class="col-3">hi</div>
+    <div class="col-6 ">hi</div>
+    <div class="col-6">hi</div>
+    <div class="col-6">hi</div>
+    <div class="col-6">hi</div>
     
 </div>
     
