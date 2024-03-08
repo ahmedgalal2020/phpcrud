1ea3c5d05414a6524ec6a7b921ae0c5443ef511f
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-08 15:34:24 +0100

    logout update

--- tasks.php
+++ tasks.php
@@ -48,6 +48,21 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id']))
 }
 ?>
 <?php include_once './parts/header.php'; ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Time Manager</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="btn btn-danger" href="./parts/logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
 <div class="container mt-5">
     <h2><?php echo $edit ? "Edit Task" : "Add New Task"; ?></h2>
