3e6138b7edec894ffb4807109cb54c3533153cec
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-08 12:20:08 +0100

    updatrergsg

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
 
 
 <?php include_once 'parts/header.php'; ?>
-<div class="container text-light bg-dark">
+<div class="container-fluid text-light bg-dark">
+<div class="container ">
 <div class="row ">
+
+
 <div class="col-6 ">
 <div class="col-4 m-1 rounded-5 p-4 border border-primary">hi</div>
     <div class="col-4 m-1 rounded-4 p-4 border border-primary">hi</div>
@@ -27,15 +30,10 @@
 
 </div>
 </div>
-
-<div class="row mt-4 text-center d-flex justify-content-center align-items-center">
-    <div class="col-4 m-1 rounded-5 p-4 border border-primary">hi</div>
-    <div class="col-4 m-1 rounded-4 p-4 border border-primary">hi</div>
-    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
-    <div class="col-6 m-1 rounded-2 p-4 border border-primary">hi</div>
-    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
-    <div class="col-12 m-1 rounded-10 p-4 border border-primary">hi</div>
-    
 </div>
+
+
+
+
     
 <?php include_once 'parts/footer.php'; ?>
