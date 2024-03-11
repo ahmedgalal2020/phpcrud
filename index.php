78e64d7566f2f6d2c14d96a00903f268524725e5
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-11 11:35:58 +0100

    update239062

--- index.php
+++ index.php
@@ -22,33 +22,9 @@ include './inc/closedb.php';
 		<div class="forms-container">
 			<div class="signin-signup">
 
-				<form action="#" class="sign-in-form">
-					<h2 class="title">Sign in</h2>
-					<div class="input-field">
-						<i class="fas fa-user"></i>
-						<input type="text" placeholder="Email" />
-					</div>
-					<div class="input-field">
-						<i class="fas fa-lock"></i>
-						<input type="password" placeholder="Password" />
-					</div>
-					<input type="submit" value="Login" class="btn solid" />
-					<p class="social-text">Or Sign in with social platforms</p>
-					<div class="social-media">
-						<a href="#" class="social-icon">
-							<i class="fab fa-facebook-f"></i>
-						</a>
-						<a href="#" class="social-icon">
-							<i class="fab fa-twitter"></i>
-						</a>
-						<a href="#" class="social-icon">
-							<i class="fab fa-google"></i>
-						</a>
-						<a href="#" class="social-icon">
-							<i class="fab fa-linkedin-in"></i>
-						</a>
-					</div>
-				</form>
+            <?php include './parts/sign-in-form.php'; ?>
+            
+				
 
 
                 <?php include './parts/sign-up-form.php'; ?>
