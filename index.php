697c694ae8d05b737f7a15d590146ee25d6bbc9e
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-11 11:09:03 +0100

    updateajgnf

--- index.php
+++ index.php
@@ -19,33 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-//logout
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
-session_start();
-
-
-
-// Unset all of the session variables.
-$_SESSION = array();
-
-// If it's desired to kill the session, also delete the session cookie.
-// Note: This will destroy the session, and not just the session data!
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-
-// Finally, destroy the session.
-session_destroy();
-
-// Redirect to login page
-header("Location: index.php");
-exit;
 
 
 
