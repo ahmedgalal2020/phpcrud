462419f82045529a5e0816b1970fc460474dec3e
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-11 11:02:03 +0100

    update24352875

--- index.php
+++ index.php
@@ -85,15 +85,7 @@ include './inc/closedb.php';
     
 <?php include_once './parts/header.php'; ?>
 
-<div class="container mt-5">
-        <h1>Welcome to the Home Page</h1>
-        <div class="row">
-            <div class="col">
-                <a href="login.php" class="btn btn-primary">Login</a>
-                <a href="register.php" class="btn btn-secondary">Register</a>
-            </div>
-        </div>
-    </div>
+
 
 
     <div class="container mt-5">
